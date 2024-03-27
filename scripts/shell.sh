3a317e1f82ea753ad1b4898e318af0485766810b
lucasdemartha <lucasribm@gmail.com>
2024-03-26 21:19:35 -0300

    teste shell

---
+++ scripts/shell.sh
@@ -0,0 +1,7 @@
+ls
+cd projetoc214
+ls
+echo "AAAHAHAHAAA"
+sudo apt-get install mailutils
+echo "Fim da instalacao"
+echo "Mandando e-mail com mail do linux" | mail -s "E-mail enviado" lucas.martha@ges.inatel.br
