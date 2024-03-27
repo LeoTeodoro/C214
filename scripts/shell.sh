364aa83b65e710f2fafbdb521e799876f44e01ea
lucasdemartha <lucasribm@gmail.com>
2024-03-26 21:57:30 -0300

    teste email

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,3 +1,4 @@
 cd projetoc214
 sudo apt-get install mailutils
-mail -s "E-mail enviado" lucas.martha@ges.inatel.br
+echo "$MAIL_BODY" | mail -s "$MAIL_SUBJECT" $MAIL_TO
+echo "Sent email to $MAIL_TO \n Subject: \n$MAIL_SUBJECT \n Body: \n$MAIL_BODY"
